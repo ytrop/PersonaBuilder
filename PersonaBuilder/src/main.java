a7c2a950c495f001a899549efb7d1f1b0d3a91d6
ytrop <62332814+ytrop@users.noreply.github.com>
2021-11-16 12:09:18 +0100

    Add files via upload

---
+++ PersonaBuilder/src/main.java
@@ -0,0 +1,16 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Persona persona = new PersonaBuilder()
+			        .email("ytrop@hotmail.com")
+			        .nombre("Jose", "Portero")
+			        .telefono("957548532")
+			        .direccion("Calle San Pedro 42").build();  
+	
+		 
+	}
+	
+
+}
